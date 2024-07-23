20636dc9ad4338b6f1c4dd41f1329cd566745b9c
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-07-24 00:48:51 +0530

    get product detail end point

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db/db";
+import { products } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { NextRequest , NextResponse } from "next/server";
+
+export async function GET(request:NextRequest , {params}:{params:{id:string}}) {
+    const id = params.id;
+
+    try {
+        const product = await db.select().from(products).where(eq(products.id , Number(id))).limit(1);
+    
+    
+        if (!product.length){
+            return Response.json({ message: 'Product not found.' }, { status: 400 });
+        }
+    
+        return Response.json(product[0])
+    } catch (error) {
+        return Response.json({ message: 'Failed to fetch a product' }, { status: 500 });
+    }
+
+}
