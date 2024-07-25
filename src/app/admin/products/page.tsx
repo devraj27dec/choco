d2053065d1296be74fa0aca8d98b5f6e15b6a6c0
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-07-25 18:52:24 +0530

    Products Table

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,8 +1,28 @@
+'use client'
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { DataTable } from './_component/dataTable'
+import { columns } from './_component/columns'
+import { useQuery } from '@tanstack/react-query'
+import { getAllProducts } from '@/http/api'
+import { Product } from '@/types'
 
 const ProductPage = () => {
+
+  const {data: products} = useQuery<Product[]>({
+    queryKey: ["products"],
+    queryFn: () => getAllProducts()
+  })
+
   return (
-    <div>ProductPage</div>
+    <>
+      <div className=' flex items-center justify-between'>
+      <h3 className=' text-xl font-bold tracking-tight'>Products</h3>
+      <Button>Add Product</Button>
+    </div>
+    
+    <DataTable columns={columns} data={products || []} />
+    </>
   )
 }
 
