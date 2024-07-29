37e3de50aa2daabb345a10b052d2055c86296575
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-07-29 13:20:05 +0530

    add Header section

--- src/app/(client)/_component/Header.tsx
+++ src/app/(client)/_component/Header.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+'use client';
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+
+  const pathname = usePathname();
+
+  const navItems = [
+    {label: 'Home' , href: '/'},
+    {label: 'Best Selling' , href: '/best-selling'},
+    {label: 'Offers' , href: '/offers'},
+    {label: 'Orders' , href: '/account/orders'}
+  ]
   return (
-    <div>Header</div>
+    <header>
+      <div className=' flex h-10 items-center justify-center text-center bg-brown-900 text-white'>
+        <span className=' text-sm'>
+          Order 2 Delight Dairy Choco bars today and save ₹100 instantly!
+        </span>
+      </div>
+      <nav className=' h-14 flex items-center justify-center'>
+        <ul className=" flex items-center justify-center gap-6">
+        {navItems.map((item) => (
+            <li key={item.href}
+              className={cn('text-brown-300 underline-offset-4 transition-all hover:cursor-pointer hover:text-brown-900 hover:underline' , pathname === item.href && 'font-semibold text-brown-900 underline' )}
+            >
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+          <li className=" text-brown-300 underline-offset-4 transition-all hover:cursor-pointer hover:text-brown-900 hover:underline  ">
+            Sign in
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './_component/Header'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Header/>
+    </>
   )
 }
 
